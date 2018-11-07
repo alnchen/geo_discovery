6a291571a87616d6991df0c75c1c8ca52b426ea7
Ed <edmundho9@gmail.com>
2018-11-06 16:30:40 -0800

    add /discover route with a google map on the page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
+import { HashRouter, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Discover from './components/discover';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <HashRouter>
+          <div>
+            <Route path="/discover/" component={Discover} />
+            <Route exact path="/">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+                <Link to="/discover/">Discover!</Link>
+              </header>
+            </Route>
+          </div>
+        </HashRouter>
       </div>
     );
   }
