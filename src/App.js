88de96e33ec6db22ace4b89765a494b57a76ed6f
Ed <edmundho9@gmail.com>
2018-11-06 16:41:00 -0800

    change link on / page to the KT photo

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Discover from './components/discover';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <HashRouter>
           <div>
             <Route path="/discover/" component={Discover} />
             <Route exact path="/">
               <header className="App-header">
-                <div>🐑:</div>
-                <img src="https://s3.amazonaws.com/s3.weei.com/s3fs-public/styles/nts_image_cover_tall_775x425/public/Klay%20Thompson.jpg?itok=juzvRjHr&c=b7b374c148cb90e1e6be464e47119fb9"></img>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-                <Link to="/discover/">Discover!</Link>
+                <Link className="App-link" to="/discover/">
+                  <span role="img" aria-label="Goat">
+                    🐑:
+                  </span>
+                  <img src="https://s3.amazonaws.com/s3.weei.com/s3fs-public/styles/nts_image_cover_tall_775x425/public/Klay%20Thompson.jpg?itok=juzvRjHr&c=b7b374c148cb90e1e6be464e47119fb9" alt="kt" />
+                </Link>
               </header>
             </Route>
           </div>
         </HashRouter>
-      </div>
-    );
+      </div>;
   }
 }
 
